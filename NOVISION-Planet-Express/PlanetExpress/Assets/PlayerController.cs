85605c147a90fac5aba8603793f716aaaa27e683
Alexandre D. Roberge <alderoberge@gmail.com>
2021-03-07 13:38:14 -0500

    Commit player controller with physics

--- NOVISION-Planet-Express/PlanetExpress/Assets/PlayerController.cs
+++ NOVISION-Planet-Express/PlanetExpress/Assets/PlayerController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Valve.VR;
+using Valve.VR.InteractionSystem;
 
 public class PlayerController : MonoBehaviour
 {
@@ -10,8 +11,19 @@ public class PlayerController : MonoBehaviour
 
     public float speed = 1;
 
+
+    private CharacterController characterController;
+
+    public void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
     public void Update()
     {
-        transform.position += speed * Time.deltaTime * new Vector3(input.axis.x, 0, input.axis.y);
+        Vector3 direction = Player.instance.hmdTransform.TransformDirection(new Vector3(input.axis.x, 0, input.axis.y));
+        Vector3 horizontalDirection = Vector3.ProjectOnPlane(direction, Vector3.up) - new Vector3(0, 9.81f, 0) * Time.deltaTime;
+
+        characterController.Move(speed * Time.deltaTime * horizontalDirection);
     }
 }
