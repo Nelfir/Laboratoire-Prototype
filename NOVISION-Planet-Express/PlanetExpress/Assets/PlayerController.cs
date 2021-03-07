371e72ac552afa7859182f011330eb73dab19f64
Alexandre D. Roberge <alderoberge@gmail.com>
2021-03-07 16:10:49 -0500

    Fixed Planet pos and scale

--- NOVISION-Planet-Express/PlanetExpress/Assets/PlayerController.cs
+++ NOVISION-Planet-Express/PlanetExpress/Assets/PlayerController.cs
@@ -14,7 +14,7 @@ public class PlayerController : MonoBehaviour
         // Fix for using both teleportation and moving at the same time
         if (input.axis.magnitude > 0.1f)
         {
-            Vector3 direction = Player.instance.hmdTransform.TransformDirection(new Vector3(input.axis.x, 0, input.axis.y));
+            Vector3 direction = Player.instance.hmdTransform.TransformDirection(new Vector3(input.axis.x, 0, input.axis.y)) * Time.deltaTime;
             Vector3 horizontalDirection = Vector3.ProjectOnPlane(direction, Vector3.up);
 
             transform.position += horizontalDirection;
