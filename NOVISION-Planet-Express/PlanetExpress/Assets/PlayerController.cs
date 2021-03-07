dd702b0e8ef86482af595e8fea1e2fbb9282446a
Alexandre D. Roberge <alderoberge@gmail.com>
2021-03-07 13:38:50 -0500

    Removed CharacterController with physics

--- NOVISION-Planet-Express/PlanetExpress/Assets/PlayerController.cs
+++ NOVISION-Planet-Express/PlanetExpress/Assets/PlayerController.cs
@@ -9,21 +9,11 @@ public class PlayerController : MonoBehaviour
 {
     public SteamVR_Action_Vector2 input;
 
-    public float speed = 1;
-
-
-    private CharacterController characterController;
-
-    public void Start()
-    {
-        characterController = GetComponent<CharacterController>();
-    }
-
     public void Update()
     {
         Vector3 direction = Player.instance.hmdTransform.TransformDirection(new Vector3(input.axis.x, 0, input.axis.y));
-        Vector3 horizontalDirection = Vector3.ProjectOnPlane(direction, Vector3.up) - new Vector3(0, 9.81f, 0) * Time.deltaTime;
+        Vector3 horizontalDirection = Vector3.ProjectOnPlane(direction, Vector3.up);
 
-        characterController.Move(speed * Time.deltaTime * horizontalDirection);
+        transform.position += horizontalDirection;
     }
 }
