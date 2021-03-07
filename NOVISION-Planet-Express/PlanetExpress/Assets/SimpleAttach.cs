8a0ec7e3b7b9e81e6f5739d8b88a18c9c6d4fe1d
Alexandre D. Roberge <alderoberge@gmail.com>
2021-03-07 13:53:57 -0500

    Added SimpleAttach script

---
+++ NOVISION-Planet-Express/PlanetExpress/Assets/SimpleAttach.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class SimpleAttach : MonoBehaviour
+{
+    private Interactable _interactable;
+
+    private void Start()
+    {
+        _interactable = GetComponent<Interactable>();
+    }
+
+    private void OnHandHoverBegin(Hand hand)
+    {
+        hand.ShowGrabHint();
+    }
+
+    private void OnHandHoverEnd(Hand hand)
+    {
+        hand.HideGrabHint();
+    }
+
+    private void OnHandHoverUpdate(Hand hand)
+    {
+        GrabTypes grabType = hand.GetGrabStarting();
+
+        bool isGrabEnding = hand.IsGrabEnding(gameObject);
+
+        if (_interactable.attachedToHand == null && grabType != GrabTypes.None)
+        {
+            hand.AttachObject(gameObject, grabType);
+            hand.HoverLock(_interactable);
+            hand.HideGrabHint();
+        }
+        else
+        {
+            hand.DetachObject(gameObject);
+            hand.HoverUnlock(_interactable);
+        }
+    }
+}
