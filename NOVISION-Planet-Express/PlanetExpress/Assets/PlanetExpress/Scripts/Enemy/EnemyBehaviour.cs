5d6ca51c78b041a61f952b930cff73a17fc31bed
Alexandre D. Roberge <alderoberge@gmail.com>
2021-03-09 14:11:50 -0500

    Fixed ships position

--- NOVISION-Planet-Express/PlanetExpress/Assets/PlanetExpress/Scripts/Enemy/EnemyBehaviour.cs
+++ NOVISION-Planet-Express/PlanetExpress/Assets/PlanetExpress/Scripts/Enemy/EnemyBehaviour.cs
@@ -1,18 +1,109 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using PlanetExpress.Scripts.Enemy;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace PlanetExpress.Scripts.Enemy
 {
-    public enum BulletType
+    public class EnemyMover : MonoBehaviour
     {
-        Normal,
-        Big
+        public Vector3 TargetPosition;
+
+
+        public float counter = 0;
+        public float duration = 20;
+
+        public Vector3 StartPosition;
+
+        public void Start()
+        {
+            StartPosition = transform.position;
+            transform.rotation = Quaternion.LookRotation(TargetPosition - transform.position);
+        }
+
+        private void FixedUpdate()
+        {
+            Debug.Log(Time.fixedDeltaTime);
+            Debug.Log(counter);
+            counter += Time.fixedDeltaTime;
+            
+            transform.position = Vector3.Lerp(StartPosition, TargetPosition, counter / duration);
+        }
+    }
+}
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    #region StaticData
+
+    /// <summary>
+    /// Generic Data
+    /// </summary>
+    public string Name;
+
+    /// <summary>
+    /// Health Data
+    /// </summary>
+    public int MaxHealth;
+
+    /// <summary>
+    /// Speed
+    /// </summary>
+    public float Speed;
+
+
+    /// <summary>
+    /// The level (damage multiplier of the enemy)
+    /// </summary>
+    public int Level;
+
+
+    /// <summary>
+    /// Bullet Data
+    /// </summary>
+    public List<GameObject> BulletSpawnPoints;
+
+    public GameObject BulletPrefab;
+
+    public float BulletDamage;
+    public float BulletFrequency;
+
+    #endregion
+
+    #region Stats
+
+    public int CurrentHealth;
+
+    public Transform TargetPosition;
+
+    #endregion
+
+    public void Start()
+    {
+        CurrentHealth = MaxHealth;
+        UpdateEnemyUI();
+        StartMove(new Vector3(0, 0, 0));
     }
 
-    public class EnemyBehaviour : MonoBehaviour
+    private void StartMove(Vector3 targetPos)
     {
-        public BulletType BulletType;
+        EnemyMover enemyMover = gameObject.AddComponent<EnemyMover>();
+        enemyMover.TargetPosition = targetPos;
+    }
 
-        public List<GameObject> BulletSpawnPoints;
+    private void UpdateEnemyUI()
+    {
+        EnemyUIController UI = GetComponentInChildren<EnemyUIController>();
+        UI.TextLevel.text = "Level " + Level;
+        UI.TextName.text = Name;
+        UI.UpdateHealth(CurrentHealth, MaxHealth);
+    }
+
+    public void Update()
+    {
+        if (TargetPosition)
+        {
+        }
     }
 }
