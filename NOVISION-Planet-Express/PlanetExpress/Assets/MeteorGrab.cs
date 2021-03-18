216db73bafece4687cf227fa1e32b1d12016466f
Alexandre D. Roberge <alderoberge@gmail.com>
2021-03-17 22:32:06 -0400

    Added MeteorGrab

---
+++ NOVISION-Planet-Express/PlanetExpress/Assets/MeteorGrab.cs
@@ -0,0 +1,27 @@
+﻿using PlanetExpress.Scripts.Currency;
+using PlanetExpress.Scripts.Planet;
+using PlanetExpress.Scripts.Shop;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class MeteorGrab : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitHandleHoverEvents();
+    }
+
+    private void InitHandleHoverEvents()
+    {
+        // Register the grab events
+        InteractableHoverEvents interactableHoverEvents = GetComponent<InteractableHoverEvents>();
+
+        interactableHoverEvents.onAttachedToHand.AddListener(() =>
+        {
+            // TODO play sound
+            CurrencyController.Instance.Value += 10;
+            Destroy(gameObject);
+        });
+    }
+}
