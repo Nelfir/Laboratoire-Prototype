7fa3d9d21f57ec5b723f0be3af5c653a65edef2e
Alexandre D. Roberge <alderoberge@gmail.com>
2021-03-30 00:22:17 -0400

    Work on UI

---
+++ NOVISION-Planet-Express/PlanetExpress/Assets/UIKit/DialogButtonController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace DefaultNamespace
+{
+    /// <summary>
+    /// Controls the Button on an Dialog.
+    /// </summary>
+    public class DialogButtonController : MonoBehaviour
+    {
+        public Sprite Blue,
+            Dark,
+            Gray,
+            Green,
+            Orange,
+            Purple,
+            Red,
+            Sky,
+            White,
+            Yellow;
+
+        public Image Background;
+        public TextMeshProUGUI Text;
+        public Button Button;
+
+        private DialogButton _dialogButton;
+
+        public void Init(DialogButton dialogButton)
+        {
+            _dialogButton = dialogButton;
+
+            // Set the text and the button callback to DialogManager
+            Text.text = dialogButton.Text;
+
+            Debug.Log("Button type : " + dialogButton.DialogButtonType);
+            
+            switch (dialogButton.DialogButtonType)
+            {
+                case DialogButtonType.Primary:
+                    SetBackground(Yellow);
+                    break;
+                case DialogButtonType.Info:
+                    SetBackground(Yellow);
+                    break;
+                case DialogButtonType.Warning:
+                    SetBackground(Orange);
+                    break;
+                case DialogButtonType.Error:
+                    SetBackground(Red);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        private void SetBackground(Sprite sprite)
+        {
+            Background.sprite = sprite;
+        }
+    }
+}
---
+++ NOVISION-Planet-Express/PlanetExpress/Assets/UIKit/DialogController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DefaultNamespace;
+using DefaultNamespace.UIKit;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DialogController : MonoBehaviour
+{
+    public TextMeshProUGUI Title;
+    public TextMeshProUGUI Description;
+
+    public GameObject AlertDialogButtonPanel;
+
+    public UnityAction<DialogButton> OnClicked { get; set; }
+
+    public void Init(Dialog dialog)
+    {
+        // Set the title and description
+        Title.text = dialog.Title;
+        Description.text = dialog.Description;
+
+        foreach (DialogButton dialogButton in dialog.Buttons)
+        {
+            // Spawn the Buttons
+
+            GameObject button = Instantiate(DialogManager.Instance.DialogButtonPrefab, AlertDialogButtonPanel.transform);
+            DialogButtonController dialogButtonController = button.GetComponent<DialogButtonController>();
+            dialogButtonController.Init(dialogButton);
+            dialogButtonController.Button.onClick.AddListener(() => { OnClicked(dialogButton); });
+        }
+    }
+}
