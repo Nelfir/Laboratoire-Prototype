b78e2ead7f4c4e1e2087a9af1b25e3e9c1bf1a9e
Alexandre D. Roberge <alderoberge@gmail.com>
2021-03-31 15:13:10 -0400

    Finalised 2D UI

--- NOVISION-Planet-Express/PlanetExpress/Assets/UIFlowController.cs
+++ NOVISION-Planet-Express/PlanetExpress/Assets/UIFlowController.cs
@@ -6,24 +6,35 @@ using UnityEngine.UI;
 
 public class UIFlowController : MonoBehaviour
 {
+    [InspectorNote("The UIs that are to be toggled on and off.")]
     public GameObject RegisterUI;
+
     public GameObject LoginUI;
     public GameObject SettingsUI;
+    public GameObject ForgotPassword;
 
-
+    [InspectorNote("The buttons that manage the UIs.")]
     public Button OnRegisterCompleted;
+    public Button OnSignupInstead;
     public Button OnLoginInstead;
     public Button OnLoginCompleted;
     public Button OnSettingsClose;
-
+    public Button OnForgotPassword;
+    public Button OnForgotPasswordCompleted;
 
     // Start is called before the first frame update
     void Start()
     {
         OnRegisterCompleted.onClick.AddListener(() => Show(LoginUI));
+
+        OnSignupInstead.onClick.AddListener(() => Show(RegisterUI));
+
         OnLoginInstead.onClick.AddListener(() => Show(LoginUI));
         OnLoginCompleted.onClick.AddListener(() => Show(SettingsUI));
         OnSettingsClose.onClick.AddListener(() => Show(null));
+
+        OnForgotPassword.onClick.AddListener(() => Show(ForgotPassword));
+        OnForgotPasswordCompleted.onClick.AddListener(() => Show(LoginUI));
 
         Show(RegisterUI);
     }
@@ -33,5 +44,6 @@ public class UIFlowController : MonoBehaviour
         RegisterUI.SetActive(show == RegisterUI);
         LoginUI.SetActive(show == LoginUI);
         SettingsUI.SetActive(show == SettingsUI);
+        ForgotPassword.SetActive(show == ForgotPassword);
     }
 }
